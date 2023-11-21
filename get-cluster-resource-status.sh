1a0f9f22901569c891d42eb39c0f0a517ea38dc1
miamayday <50498761+miamayday@users.noreply.github.com>
2023-11-21 13:03:09 +0200

    Update get-cluster-resource-status.sh

--- get-cluster-resource-status.sh
+++ get-cluster-resource-status.sh
@@ -12,7 +12,7 @@ add_to_total() {
 
 kubectl get node | tail -n +2 | awk '{print $1}' > "${workspace}"/nodes.tmp
 
-printf 'NODE USED_CPU USED_MEM UNUSED_CPU UNUSED_MEM RESERVED_CPU UNRESERVED_CPU\n'
+printf 'NODE USED_CPU USED_MEM UNUSED_CPU UNUSED_MEM RESERVED_CPU RESERVED_MEM UNRESERVED_CPU UNRESERVED_MEM\n'
 
 total_used_cpu_base=0
 total_used_mem_gi=0
@@ -20,7 +20,8 @@ total_unused_cpu_base=0
 total_unused_mem_gi=0
 total_reserved_cpu_base=0
 total_reserved_mem_gi=0
-total_unused_cpu_base=0
+total_unreserved_cpu_base=0
+total_unreserved_mem_gi=0
 while IFS= read -r node
 do
   "${workspace}"/get-node-resource-status.sh "${node}" > status.tmp
@@ -30,6 +31,8 @@ do
   unused_mem_gi="$(grep 'Unused:' status.tmp | awk '{print $4}' | grep -oE '^(\.|[0-9])+')"
   reserved_cpu_base="$(grep 'Reserved:' status.tmp | awk '{print $2}' | grep -oE '^(\.|[0-9])+')"
   reserved_mem_gi="$(grep 'Reserved:' status.tmp | awk '{print $4}' | grep -oE '^(\.|[0-9])+')"
+  unreserved_cpu_base="$(grep 'Unreserved:' status.tmp | awk '{print $2}' | grep -oE '^(\.|[0-9])+')"
+  unreserved_mem_gi="$(grep 'Unreserved:' status.tmp | awk '{print $4}' | grep -oE '^(\.|[0-9])+')"
   
   total_used_cpu_base="$(echo "${total_used_cpu_base}+${used_cpu_base}" | bc -l)"
   total_used_mem_gi="$(echo "${total_used_mem_gi}+${used_mem_gi}" | bc -l)"
@@ -37,8 +40,10 @@ do
   total_unused_mem_gi="$(echo "${total_unused_mem_gi}+${unused_mem_gi}" | bc -l)"
   total_reserved_cpu_base="$(echo "${total_reserved_cpu_base}+${reserved_cpu_base}" | bc -l)"
   total_reserved_mem_gi="$(echo "${total_reserved_mem_gi}+${reserved_mem_gi}" | bc -l)"
+  total_unreserved_cpu_base="$(echo "${total_unreserved_cpu_base}+${unreserved_cpu_base}" | bc -l)"
+  total_unreserved_mem_gi="$(echo "${total_unreserved_mem_gi}+${unreserved_mem_gi}" | bc -l)"
 
-  printf '%s %.1f %.2fGi %.1f %.2fGi %.1f %.2fGi\n' "${node}" "${used_cpu_base}" "${used_mem_gi}" "${unused_cpu_base}" "${unused_mem_gi}" "${reserved_cpu_base}" "${reserved_mem_gi}"
+  printf '%s %.1f %.2fGi %.1f %.2fGi %.1f %.2fGi %.1f %.2fGi\n' "${node}" "${used_cpu_base}" "${used_mem_gi}" "${unused_cpu_base}" "${unused_mem_gi}" "${reserved_cpu_base}" "${reserved_mem_gi}" "${unreserved_cpu_base}" "${unreserved_mem_gi}"
 done < nodes.tmp
 
-printf 'Total %.1f %.2fGi %.1f %.2fGi %.1f %.2fGi\n' "${total_used_cpu_base}" "${total_used_mem_gi}" "${total_unused_cpu_base}" "${total_unused_mem_gi}" "${total_reserved_cpu_base}" "${total_reserved_mem_gi}"
+printf 'Total %.1f %.2fGi %.1f %.2fGi %.1f %.2fGi %.1f %.2fGi\n' "${total_used_cpu_base}" "${total_used_mem_gi}" "${total_unused_cpu_base}" "${total_unused_mem_gi}" "${total_reserved_cpu_base}" "${total_reserved_mem_gi}" "${total_unreserved_cpu_base}" "${total_unreserved_mem_gi}"
