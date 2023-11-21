6a5899b78144498d607f82f260821e9674dd8f3f
miamayday <50498761+miamayday@users.noreply.github.com>
2023-11-21 12:52:33 +0200

    Update get-cluster-resource-status.sh

--- get-cluster-resource-status.sh
+++ get-cluster-resource-status.sh
@@ -28,13 +28,17 @@ do
   used_mem_gi="$(grep 'Used:' status.tmp | awk '{print $4}' | grep -oE '^(\.|[0-9])+')"
   unused_cpu_base="$(grep 'Unused:' status.tmp | awk '{print $2}' | grep -oE '^(\.|[0-9])+')"
   unused_mem_gi="$(grep 'Unused:' status.tmp | awk '{print $4}' | grep -oE '^(\.|[0-9])+')"
+  reserved_cpu_base="$(grep 'Reserved:' status.tmp | awk '{print $2}' | grep -oE '^(\.|[0-9])+')"
+  reserved_mem_gi="$(grep 'Reserved:' status.tmp | awk '{print $4}' | grep -oE '^(\.|[0-9])+')"
   
   total_used_cpu_base="$(echo "${total_used_cpu_base}+${used_cpu_base}" | bc -l)"
   total_used_mem_gi="$(echo "${total_used_mem_gi}+${used_mem_gi}" | bc -l)"
   total_unused_cpu_base="$(echo "${total_unused_cpu_base}+${unused_cpu_base}" | bc -l)"
   total_unused_mem_gi="$(echo "${total_unused_mem_gi}+${unused_mem_gi}" | bc -l)"
+  total_reserved_cpu_base="$(echo "${total_reserved_cpu_base}+${reserved_cpu_base}" | bc -l)"
+  total_reserved_mem_gi="$(echo "${total_reserved_mem_gi}+${reserved_mem_gi}" | bc -l)"
 
-  printf '%s %.1f %.2fGi %.1f %.2fGi\n' "${node}" "${used_cpu_base}" "${used_mem_gi}" "${unused_cpu_base}" "${unused_mem_gi}"
+  printf '%s %.1f %.2fGi %.1f %.2fGi %.1f %.2fGi\n' "${node}" "${used_cpu_base}" "${used_mem_gi}" "${unused_cpu_base}" "${unused_mem_gi}" "${reserved_cpu_base}" "${reserved_mem_gi}"
 done < nodes.tmp
 
-printf 'Total %.1f %.2fGi %.1f %.2fGi\n' "${total_used_cpu_base}" "${total_used_mem_gi}" "${total_unused_cpu_base}" "${total_unused_mem_gi}"
+printf 'Total %.1f %.2fGi %.1f %.2fGi %.1f %.2fGi\n' "${total_used_cpu_base}" "${total_used_mem_gi}" "${total_unused_cpu_base}" "${total_unused_mem_gi}" "${total_reserved_cpu_base}" "${total_reserved_mem_gi}"
