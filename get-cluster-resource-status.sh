759c142e31ebc03ec6845b6b5112a85edf6fc5d9
miamayday <50498761+miamayday@users.noreply.github.com>
2023-11-21 12:41:17 +0200

    Update get-cluster-resource-status.sh

--- get-cluster-resource-status.sh
+++ get-cluster-resource-status.sh
@@ -12,13 +12,26 @@ add_to_total() {
 
 kubectl get node | tail -n +2 | awk '{print $1}' > "${workspace}"/nodes.tmp
 
+total_used_cpu_base=0
+total_used_mem_gi=0
+total_unused_cpu_base=0
+total_unused_mem_gi=0
+total_reserved_cpu_base=0
+total_reserved_mem_gi=0
+total_unused_cpu_base=0
 while IFS= read -r node
 do
   "${workspace}"/get-node-resource-status.sh "${node}" > status.tmp
+  used_cpu_base="$(grep 'Used:' status.tmp | awk '{print $2}' | grep -oE '^(\.|[0-9])+')"
+  used_mem_gi="$(grep 'Unused:' status.tmp | awk '{print $4}' | grep -oE '^(\.|[0-9])+')"
+  unused_cpu_base="$(grep 'Unused:' status.tmp | awk '{print $2}' | grep -oE '^(\.|[0-9])+')"
+  unused_mem_gi="$(grep 'Unused:' status.tmp | awk '{print $4}' | grep -oE '^(\.|[0-9])+')"
+  
+  total_used_cpu_base="$(echo "${total_used_cpu_base}+${used_cpu_base}" | bc -l)"
+  total_used_mem_gi="$(echo "${total_used_mem_gi}+${used_mem_gi}" | bc -l)"
+  total_unused_cpu_base="$(echo "${total_unused_cpu_base}+${unused_cpu_base}" | bc -l)"
+  total_unused_mem_gi="$(echo "${total_unused_mem_gi}+${unused_mem_gi}" | bc -l)"
 done < nodes.tmp
 
-exit
-
-printf "Total
-
-rm -f reserved_cpu_base.total reserved_mem_gi.total used_cpu_base.total used_mem_gi.total
+printf 'Total used: %.1f CPU %.2fGi RAM\n' "${total_used_cpu_base}" "${total_used_mem_gi}"
+printf 'Total unused: %.1f CPU %.2fGi RAM\n' "${total_unused_cpu_base}" "${total_unused_mem_gi}"
